10c1c7ebef1a35e0e4c1deb5b736fc59a1040342
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-12 10:52:52 -0500

    Update main.js

--- quantum_harmonic_oscillators_on_complex_plane/main.js
+++ quantum_harmonic_oscillators_on_complex_plane/main.js
@@ -85,9 +85,9 @@ function main()
     var y_data_4 = [];
     var x_data_5 = [];
     var y_data_5 = [];
-	var x_min = -3.0;
-	var x_max = 3.0;
-	var dx = 0.01;
+    var x_min = -3.0;
+    var x_max = 3.0;
+    var dx = 0.01;
     var num_points = Math.floor((x_max - x_min)/dx);
     var x;
     var w_e = Math.pow(10, -3);  //angular velocity of electron
